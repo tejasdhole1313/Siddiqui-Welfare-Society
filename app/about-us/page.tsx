a5bb2413c7e0fde2534da927c9cbf259eed9f736
tejasdhole1313 <tejasbd2004@gmail.com>
2025-09-03 19:04:19 +0530

    home page change image

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -300,7 +300,7 @@ useEffect(() => {
     {/* Activity 1 */}
     <div>
       <Image
-        src="/images/Acitivity01.png" 
+        src="./images/Acitivity01.png" 
         alt="Aiding small businesses"
         width={1200}
         height={400}
@@ -314,7 +314,7 @@ useEffect(() => {
     {/* Activity 2 */}
     <div>
       <Image
-        src="/images/Acitivity02.jpg" 
+        src="./images/Acitivity02.jpg" 
         alt="Career guidance camps"
         width={1200}
         height={400}
@@ -328,7 +328,7 @@ useEffect(() => {
     {/* Activity 3 */}
     <div>
       <Image
-        src="/images/Acitivity03.jpg"
+        src="./images/Acitivity03.jpg"
         alt="Food distribution"
         width={1200}
         height={400}
